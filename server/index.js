68b12efba8eb5caeb1a92f9cdcba805ea30fc51c
dhananjaykumare2003 <dhananjaykumare3@gmail.com>
2025-08-25 11:45:17 +0530

    made one change

--- server/index.js
+++ server/index.js
@@ -52,7 +52,7 @@ app.use('/api/messages', messageRouter);
 dbConnect();
 
 if(process.env.NODE_ENV !== "production"){
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 4000;
     server.listen(PORT, () => {
         console.log(`App is running at PORT : ${PORT}`);
     })
